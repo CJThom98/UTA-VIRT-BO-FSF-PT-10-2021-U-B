7ccdbb55682e8700b686898ceb21bc9823f6cd9d
Cory Thompson <cory.thompson98@gmail.com>
2021-11-30 16:55:45 -0600

    Update script.js

--- 04-Web-APIs/01-Activities/02-Event-Listeners/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/02-Event-Listeners/Unsolved/assets/js/script.js
@@ -1,21 +1,25 @@
 var count = 0;
 
 // TODO: Add a comment describing the functionality of the following document.querySelector() methods:
+// Finding the Ids of the button elements
 var incrementEl = document.querySelector('#increment');
 var decrementEl = document.querySelector('#decrement');
 var countEl = document.querySelector('#count');
 
 // TODO: Add a comment describing the functionality of the following code:
+// Starts count at 0
 function setCounterText() {
   countEl.textContent = count;
 }
 // TODO: Add a comment describing the functionality of the following event listener:
+// Listening for click on the increment
 incrementEl.addEventListener('click', function() {
   count++;
   setCounterText();
 });
 
 // TODO: Add a comment describing the functionality of the following event listener:
+// Listens for click to descrease number is >0
 decrementEl.addEventListener('click', function() {
   // Action will fire if count is greater than  0
   if (count > 0) {
