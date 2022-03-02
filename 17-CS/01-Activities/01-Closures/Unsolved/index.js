defc71710542bc9684dbce3594e1fe6e3420f114
Cory Thompson <cory.thompson98@gmail.com>
2022-03-02 12:11:21 -0600

    hello

--- 17-CS/01-Activities/01-Closures/Unsolved/index.js
+++ 17-CS/01-Activities/01-Closures/Unsolved/index.js
@@ -1,5 +1,20 @@
 // TODO: Complete the 'counter' function below.
-function counter() {}
+function counter(a) {
+    let numOne = a;
+    return {
+        hiddenCount: function(b) {
+            let numTwo = b;
+            return count ++;
+        }
+    };
+}
+
+const newCount = counter(5);
+
+const notherCounter = counter(2);
+
+console.log('here is new count', newCount.increment(3));
+console.log('nother count', notherCounter.increment(2));
 
 // Displays an interactive list of the properties of the specified JavaScript object
 console.dir(counter);
--- 17-CS/01-Activities/02-Callback-Queue/Unsolved/assets/js/script.js
+++ 17-CS/01-Activities/02-Callback-Queue/Unsolved/assets/js/script.js
@@ -1,9 +1,15 @@
-// function to block the stack for x number of milliseconds
-const pause = milliSeconds => {
-  // get the current time
-  var startTime = new Date().getTime(); 
-  // block stack until time's up
-  while (new Date().getTime() < startTime + milliSeconds); 
-}
+// // function to block the stack for x number of milliseconds
+// const pause = milliSeconds => {
+//   // get the current time
+//   var startTime = new Date().getTime(); 
+//   // block stack until time's up
+//   while (new Date().getTime() < startTime + milliSeconds); 
+// }
 
-pause(8000);
+// pause(8000);
+
+//! wait 8 seconds in a separate thread
+setTimeout(() => {
+  document.querySelector('button')
+  classList.remove('display-area');
+}.8000)
