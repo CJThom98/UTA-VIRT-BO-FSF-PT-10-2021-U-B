7e77e6446364353c263158e83c6c50e87cba5ccb
Cory Thompson <cory.thompson98@gmail.com>
2022-03-10 22:34:43 -0600

    Update User.js

--- 18-NoSQL/01-Activities/03-Mongoose-Schema/Unsolved/models/User.js
+++ 18-NoSQL/01-Activities/03-Mongoose-Schema/Unsolved/models/User.js
@@ -4,6 +4,27 @@ const UserSchema = new Schema({
   // Add these four attributes to your schema: username, password, email, userCreated
   // YOUR CODE HERE
   //
+
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    required: 'Username is Required',
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: 'at least 6 characters',
+    minLength: 6,
+  },
+  email: {
+    type: String,
+    match: [/.+@.+\..+/],
+  },
+  userCreated: {
+    data: Date,
+    default: Date.now,
+  },
 });
 
 const User = model('User', UserSchema);
